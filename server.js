77ec66e97dd9c54669da9dba47a3e5c921006517
LuisMiguelRodriguez <luis.the.coder@gmail.com>
2019-07-06 02:00:12 -0500

    heroku deployment setup

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
 
 const app = express();
+const PORT = process.env.PORT || 3001;
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
@@ -13,6 +15,15 @@ app.get('/api/greeting', (req, res) => {
   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
 
-app.listen(3001, () =>
-  console.log('Express server is running on localhost:3001')
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+app.listen(PORT, () =>
+  console.log(`Express server is running on localhost:${PORT}`)
 );

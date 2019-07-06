95fdb590ad8eaf1df3b99f29c9c9b2d09eec4922
LuisMiguelRodriguez <luis.the.coder@gmail.com>
2019-07-06 02:09:48 -0500

    add path package

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
